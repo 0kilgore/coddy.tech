af6369e6c7514d514822a0b188dc16b6afdf1196
ProjectHazel <razorstech@gmail.com>
2023-03-21 04:05:50 -0500

    update 1.2

--- challenge.js
+++ challenge.js
@@ -8,3 +8,10 @@ function math(num1,num2){
 }
 
 // 
+// You are given two numbers, num1 and num2. Calculate and print their sum. Then, print the smaller number of the two. At the end, print the first number to the power of 2. Use the Math object for this challenge.
+
+function math(num1,num2){
+    console.log(num1+num2);
+    console.log(Math.pow(num1,2))
+    console.log(Math.min(num1,mum2));
+}
