e49cbb5f5c0a698a46103a4469f262f6353b1e9c
ProjectHazel <razorstech@gmail.com>
2023-03-21 03:33:02 -0500

    coddy.tech js course

---
+++ math.js
@@ -0,0 +1,20 @@
+// simple math from coddy.tech 
+
+//simple math with numbers 
+
+const num1 = 100;
+const num2 = 50;
+let value;
+
+// add and subtraction 
+
+value = num1 + num2; // value of 150 
+value = num1 - num1; // value of 50 
+
+// Multiplication & Division 
+
+value = num1 * num2; // value of 5000
+value = num1 / num2; // value 2 
+
+//Modulo we use % symbol to calculate the remainder from dividing two number 
+value = num1 % num2; // value 0 
