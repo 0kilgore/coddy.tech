54b765ea82ead0db12524edcb0c4da3745225342
ProjectHazel <razorstech@gmail.com>
2023-03-21 03:53:27 -0500

    update

---
+++ challenge.js
@@ -0,0 +1,10 @@
+//You are given two numbers num1 and num2. Print the sum, difference, product and the quotient.
+
+function math(num1,num2){
+    console.log(num1+num2);
+    console.log(num1-num2);
+    console.log(num1 * num2);
+    console.log(num1 / num2);
+}
+
+// 
--- math.js
+++ math.js
@@ -18,3 +18,46 @@ value = num1 / num2; // value 2
 
 //Modulo we use % symbol to calculate the remainder from dividing two number 
 value = num1 % num2; // value 0 
+
+
+// number & the Math Object 
+// the Math object 
+//In JavaScript, there is an object called Math that can be used for more complex mathematics. It has multiple methods that we can use:
+
+// getting the value of PI 
+
+value = Math.PI; // Output: 3.141592
+
+// rounding a number 
+
+value = Math.round(2.4);// out 2
+
+//Rounding down to the nearest whole number
+
+value = Math.floor(3.67); // Output: 3
+
+//Rounding up to the nearest whole number
+
+value = Math.ceil(2.1); // Output: 3
+
+//Calculating square root
+
+value = Math.sqrt(64); // Output: 8
+
+//Absolute value
+
+value = Math.abs(-5); // Output: 5
+
+// Powers (First number to the power of second number)
+value = Math.pow(8, 2); // Output: 64
+
+// Minimum
+value = Math.min(5, 2, 1, 4, 6); // Output: 1
+
+//Maximum
+
+value = Math.max(2, 3, 1, 5, 4); // Output: 5
+
+// Random value
+
+value = Math.random() // Output: random decimal number from 0 to 1;
